a40e39384b20419942d82b0af42dc010778447d3
SUAREZ GONZALEZ <alu4308@cc328.(none)>
2012-11-02 16:23:14 +0000

    Test 1

---
+++ lib/racional.rb
@@ -0,0 +1,10 @@
+#
+# racional.rb
+# Clase Racional
+#
+
+class Racional
+
+  attr_reader :numerador
+  
+end
---
+++ test/racional_spec.rb
@@ -0,0 +1,20 @@
+#
+# racional_spec.rb
+# Spec para la Clase Racional
+#
+
+require 'rspec'
+require 'racional'
+
+describe Racional do
+  
+  before :each do
+      @racional = Racional.new
+  end
+  
+  it "Debe existir un numerador" do
+    @racional.respond_to?("numerador").should == true
+  end
+  
+end
+
