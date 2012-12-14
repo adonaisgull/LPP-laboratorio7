dbf935cd76e35d237049007430f2c9dbbcab7f88
SUAREZ GONZALEZ <alu4308@cc331.(none)>
2012-12-14 16:18:04 +0000

    Test 3

--- lib/racional.rb
+++ lib/racional.rb
@@ -7,4 +7,19 @@ class Racional
 
   attr_reader :numerador, :denominador
   
+  def initialize(num, denom)
+    @numerador, @denominador = num, denom
+    reducir
+  end
+
+  def reducir
+    div = mcd(@numerador, @denominador)
+    @numerador = @numerador / div
+    @denominador = @denominador / div
+  end
+  
+  def mcd(a, b)
+    return a if b == 0
+    mcd(b, a % b)
+  end 
 end
